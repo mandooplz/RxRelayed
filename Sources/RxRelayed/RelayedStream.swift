2eee908c84962a96fe033535dac97ea3f4a5e159
kmw4561@gmail.com <kmw4561@gmail.com>
2026-01-02 20:28:58 +0900

    RelayedStream 추가 & RxCombine 통합

---
+++ Sources/RxRelayed/RelayedStream.swift
@@ -0,0 +1,21 @@
+/// Rx와 Combine 스트림을 선택적으로 제공하는 Proxy 구조체
+public struct RelayedStream<T> {
+    private let relay: BehaviorRelay<T>
+    
+    init(relay: BehaviorRelay<T>) {
+        self.relay = relay
+    }
+    
+    /// RxSwift의 Driver 반환
+    public var driver: Driver<T> {
+        return relay.asDriver()
+    }
+    
+    /// RxCombine을 활용하여 BehaviorRelay를 Combine Publisher로 즉시 변환
+    public var publisher: AnyPublisher<T, Never> {
+        return relay.asObservable()
+            .asPublisher() // RxCombine의 확장 메서드
+            .catch { _ in Empty<T, Never>() } // 에러 타입 일치 (Never)
+            .eraseToAnyPublisher()
+    }
+}
